3b7a33a8639f099ef752b3b254ff023607bed6b5
jbar <jeanjacquesbrucker@gmail.com>
2011-06-12 08:04:55 +0200

    working on initialisation of money

---
+++ scripts/initsig.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if gpg2 --version 2> /dev/null ; then 
+    gpg="gpg2"
+elif gpg --version 2> /dev/null ; then
+    gpg="gpg"
+else
+    echo -e "no gpg found in your \$PATH ($PATH)\n"
+            "please install GnuPG (http://www.gnupg.org/)"
+    exit -1
+fi
+
+mykeys=($(gpg2 --list-secret-keys --with-colons | grep "^s" | cut -d: -f 5))
+
+if gpg2 --list-secret-keys 2> /dev/null | grep -o "udid2;h;[[:xdigit:]]\{,40\};[0-9]\+;" ; then 
+    myudid=$(gpg2 --list-secret-keys 2> /dev/null | grep -o -m 1 "udid2;h;[[:xdigit:]]\{,40\};[0-9]\+;")
+elif gpg2 --list-secret-keys 2> /dev/null | grep -o "udid2;c;[A-Z-]\{,20\};[A-Z-]\{,20\};[0-9-]\{10\};e[0-9.+-]\{13\};[0-9]\+;" ; then 
+    myudid=$(gpg2 --list-secret-keys 2> /dev/null | grep -o -m 1 "udid2;c;[A-Z-]\{,20\};[A-Z-]\{,20\};[0-9-]\{10\};e[0-9.+-]\{13\};[0-9]\+;")
+elif gpg2 --list-secret-keys 2> /dev/null | grep -o "udid1;[^;]\+;[^;]\+;[^;]\+;[^;]\+;" ; then
+    myudid=$(gpg2 --list-secret-keys 2> /dev/null | grep -o -m 1 "udid1;[^;]\+;[^;]\+;[^;]\+;[^;]\+;")
+else
+    echo -e "no udid found in your private datas\n"
+            "please create a valid udid2 and make it signed by some confidents contacts"
+    exit -2
+fi
+
+
