3bfef1275e7a5afaaa162d3ac0b6a110b03689df
jbar <jeanjacquesbrucker@gmail.com>
2011-07-03 03:22:39 +0200

    Geo list for French metropole cities created

--- scripts/gen_coordinates_frmetropole.sh
+++ scripts/gen_coordinates_frmetropole.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+grep landmark docs/RGC_arrondissements/arr_paris | sed ' s/.*pagename=\([0-9]*\).*params=\([^_]*\)_N_\([^_]*\).*/\1 \2 \3/ ' | tr '.' ',' | while read arr lat long ; do printf "FRANCE\tPARIS $arr (75)\te+%.3f+00%.3f\n" $lat $long | tr ',' '.' ; done
+#echo -e "FRANCE\tPARIS 4 (75)\te+48.86+002.35" # This doesn't not match registery to be different from PARIS 3
+#echo -e "FRANCE\tPARIS 9 (75)\te+48.88+002.34" # This doesn't not match registery to be different from PARIS 2
+
 export IFS="$(echo -e "\t")"
-#grep -i julien docs/RGC/RGC_2010.txt | while read DEP     COM     ARRD    CANT    ADMI    POPU    SURFACE NOM     XLAMB2  YLAMB2  XLAMBZ  YLAMBZ  XLAMB93 YLAMB93 LONGI_GRD       LATI_GRD        LONGI_DMS       LATI_DMS      ZMIN    ZMAX ; do 
-tail -n +2 docs/RGC/RGC_2010.txt | while read DEP     COM     ARRD    CANT    ADMI    POPU    SURFACE NOM     XLAMB2  YLAMB2  XLAMBZ  YLAMBZ  XLAMB93 YLAMB93 LONGI_GRD       LATI_GRD        LONGI_DMS       LATI_DMS      ZMIN    ZMAX ; do 
+#grep -i paris docs/RGC/RGC_2010.txt | while read DEP     COM     ARRD    CANT    ADMI    POPU    SURFACE NOM     XLAMB2  YLAMB2  XLAMBZ  YLAMBZ  XLAMB93 YLAMB93 LONGI_GRD       LATI_GRD        LONGI_DMS       LATI_DMS      ZMIN    ZMAX ; do 
+tail -n +2 docs/RGC/RGC_2010.txt | grep -v "^75.*\<PARIS\>" | while read DEP     COM     ARRD    CANT    ADMI    POPU    SURFACE NOM     XLAMB2  YLAMB2  XLAMBZ  YLAMBZ  XLAMB93 YLAMB93 LONGI_GRD       LATI_GRD        LONGI_DMS       LATI_DMS      ZMIN    ZMAX ; do 
+    if (($ADMI==6)) ; then continue ; fi ;
     LONGsign="${LONGI_DMS:0:1}" ;
     if ((${#LONGI_DMS}==6)) ; then 
         LONGdeg=$( printf "%03d\n" ${LONGI_DMS:1:1} ) ; 
@@ -13,19 +18,17 @@ tail -n +2 docs/RGC/RGC_2010.txt | while read DEP     COM     ARRD    CANT    AD
         LONGmin=${LONGI_DMS:3:2} ; 
         LONGsec=${LONGI_DMS:5:2} ; 
     else 
-        echo "Oups, longitude non gérée ($NOM ${LONGI_DMS})" >&2 ; exit ; 
+        echo "Oups, longitude non gérée ($NOM ($DEP) ${LONGI_DMS})" >&2 ; continue ; 
     fi ; 
     if ((${#LATI_DMS}==6)) ; then 
         LATdeg=$( printf "%02d\n" ${LATI_DMS:0:2} ) ; 
         LATmin=${LATI_DMS:2:2} ; 
         LATsec=${LATI_DMS:4:2} ; 
     else 
-        echo "Oups, latitude non gérée ($NOM ${LATI_DMS})" >&2 ; exit ; 
+        echo "Oups, latitude non gérée ($NOM ($DEP) ${LATI_DMS})" >&2 ; continue ; 
     fi ; 
-    LATdec=$(echo " $LATmin/60 + $LATsec/3600 " | bc -l | grep -o "\..." ) ;
-    #LATdec=$(awk -v var="$LATdec" 'BEGIN{ printf"%.2f\n", var}');
-    LONGdec=$(echo " $LONGmin/60 + $LONGsec/3600 " | bc -l | grep -o "\..." ) ; 
-    #LONGdec=$(awk -v var="$LONGdec" 'BEGIN{ printf"%.2f\n", var}');
-    echo -e "$DEP\t$COM\t$ADMI\t$NOM\te+$LATdeg$LATdec$LONGsign$LONGdeg$LONGdec" ; 
+    LATdec=$(awk -v LATmin="$LATmin" -v LATsec="$LATsec" 'BEGIN{ printf"%.3f\n", LATmin/60+LATsec/3600}');
+    LONGdec=$(awk -v LONGmin="$LONGmin" -v LONGsec="$LONGsec" 'BEGIN{ printf"%.3f\n", LONGmin/60+LONGsec/3600}');
+    echo -e "FRANCE\t$NOM ($DEP)\te+$LATdeg${LATdec:1}$LONGsign$LONGdeg${LONGdec:1}" ; 
 done
 
