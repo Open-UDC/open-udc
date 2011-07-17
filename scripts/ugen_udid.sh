4cb034bc5c34eb7b68d95249c5f299e2facfb1ea
jbar <jeanjacquesbrucker@gmail.com>
2011-07-17 10:09:59 +0200

    working on the tool to generate udid2

--- scripts/ugen_udid.sh
+++ scripts/ugen_udid.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 SW="OpenUDC - devel"
+UDCHOME="$HOME/.openudc"
+KEYSERVER="keys.gnupg.net"
+GEOLISTFPR=("0F16B563D2768EA62B36A13C442C7E45EEF5EAE6")
+GEOLISTUDID=("udid2;h;4c5441eb5fbe391b27f6baaa1e8203d1990d98b5;0")
+
 Version="ugen_udid.sh 0.0.1 ($SW)"
 
-Countries=( "FRA" )
+Countries=("FRA")
+for i in ${!Countries[@]} ; do 
+    CFiles[$i]="$UDCHOME/udid2/geolist_${Countries[$i]}.txt.asc"
+done
 
 function usage {
 echo -e "Usage: $0 [options]\n"\
      "Options:\n"\
      " -h, --help\t\tthis help"\
      " -V, --version\t\tversion"\
-     " -f, --file\t\tgeolist file to use" > &2
+     " -f, --file\t\tgeolist file to use" >&2
     exit $1
 }
 
@@ -19,9 +27,13 @@ if gpg2 --version 2> /dev/null | head -n 3 ; then
 elif gpg --version 2> /dev/null | head -n 3 ; then
     gpg="gpg"
 else
-    echo -e "\nWarning: No gpg found in your \$PATH ($PATH)\n"\
+    echo -e "\nError: No gpg found in your \$PATH ($PATH)\n"\
             "please install GnuPG (http://www.gnupg.org/)\n" >&2
-    gpg=false
+    exit -3
+fi
+
+if ! $gpg --list-key "${GEOLISTUDID[0]}" 2> /dev/null > /dev/null ; then
+    $gpg gpg2 --recv-keys --batch --no-verbose --keyserver "$KEYSERVER" "$GEOLISTFPR"
 fi
 
 for ((i=0;$#;)) ; do 
@@ -30,17 +42,53 @@ for ((i=0;$#;)) ; do
         -V|--vers*) echo $Version ; exit ;;
         -f|--f*)
             shift
-            if [ -f "$1" ] ; then
-                Country=$(head -n 1 "$1" | cut -d "$(printf "\t")" -f 2 ) 
-                GeoFiles[$COUNTRY]="$1"
+            if [ -f "$1" ] && Country="$(sed -n '4s,e[0-9+.]\+\t\([A-Z]\{3\}\)\t.*,\1,p' "$1" )" && [ "$Country" ] ; then
+                cCountries[$i]="$Country"
+                cCFiles[$((i++))]="$1"
+                Countries="${cCountries[@]}"
+                CFiles="${cCFiles[@]}"
             else
-                echo -e "Error: geolist file $1 does not exist\n" ; usage -1;
+                echo "Error: incorrect geolist file $1" >&2 ; usage -1;
             fi ;;
+        *) echo "Error: Unrecognized option $1" >&2 ; usage -1;;
     esac
     shift
 done
 
-if ! LANGUAGE=en $gpg --verify ${GeoFile:=geolist_FRA.txt.asc} 2>&1 | grep -o 'Good signature from ".\+(udid2;h;4c5441eb5fbe391b27f6baaa1e8203d1990d98b5;0\>' ; then
-    echo "Warning: geolist signature ..."
+function chooseintab {
+    ret=0
+    tabs=$(eval echo "\${#$2[@]}")
+    if ((tabs==1)) ; then
+        echo "$(eval echo "\${$2[0]}") $3"
+        return 1
+    fi
+    echo -n "$1"
+    for i in $(eval echo \${!$2[@]}) ; do
+        ((i%3)) || echo && echo -en "\t\t"
+        echo -en "$((i+1))) $(eval echo "\${$2[$i]}")"
+    done
+    echo
+    while ((ret<1 || ret>tabs)) ; do
+        read -p "Reply (1-$tabs) ? " ret
+    done
+    return $ret
+}
+
+
+Countries[${#Countries[@]}]="Other..."
+chooseintab "Please select your Country of Birth..." Countries
+ret=$?
+if ((ret==${#Countries[@]})) ; then
+    echo -e " Sorry: we can't generate your udid.\n"\
+            "Please join the OpenUDC's developpement team to add support for your birthplace <open-udc@googlegroups.com>."
+    exit
+else
+    GFile="${CFiles[((ret-1))]}"
 fi
+
+if ! LANGUAGE=en $gpg --verify --no-verbose --batch "$GFile" 2>&1 | grep -o "(${GEOLISTUDID[0]}\>.*)" ; then
+    echo "Warning: geolist signature ..." >&2
+    #Note: Trust is not checked.
+fi
+
 
