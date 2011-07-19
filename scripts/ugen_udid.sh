9dfc52a133a19536395728daf65df33d30b9839f
jbar <jeanjacquesbrucker@gmail.com>
2011-07-19 09:46:11 +0200

    ...

--- scripts/ugen_udid.sh
+++ scripts/ugen_udid.sh
@@ -43,7 +43,7 @@ for ((i=0;$#;)) ; do
         -f|--f*)
             shift
             if [ -f "$1" ] \
-                    && Country="$($gpg --no-verbose --batch --decrypt "$1" 2> /dev/null | sed -n '2s,e[0-9+.]\+\t\([A-Z]\{3\}\)\t.*,\1,p' ;)" \
+                    && Country="$($gpg --no-verbose --batch --decrypt "$1" 2> /dev/null | sed -n '2s,e[0-9.+-]\+\t\([A-Z]\{3\}\)\t.*,\1,p' ;)" \
                     && [ "$Country" ] ; then
                     #Note: The validity of the signature will be checked later in the script
                 cCountries[$i]="$Country"
@@ -58,28 +58,30 @@ for ((i=0;$#;)) ; do
     shift
 done
 
-function chooseintab {
+function chooseinlist {
     ret=0
-    tabs=$(eval echo "\${#$2[@]}")
-    if ((tabs==1)) ; then
-        echo "$(eval echo "\${$2[0]}") $3"
-        return 1
-    fi
     echo -n "$1"
-    for i in $(eval echo \${!$2[@]}) ; do
-        ((i%3)) || echo && echo -en "\t\t"
-        echo -en "$((i+1))) $(eval echo "\${$2[$i]}")"
+    shift
+    n=$#
+    for ((i=0;$#;)) ; do
+        if ((i%3)) ; then 
+            echo -en "\t\t"
+        else
+            echo -en "\n\t"
+        fi
+        echo -en "$((++i))) $1"
+        shift
     done
     echo
-    while ((ret<1 || ret>tabs)) ; do
-        read -p "Reply (1-$tabs) ? " ret
+    while ((ret<1 || ret>n)) ; do
+        read -p "Reply (1-$n) ? " ret
     done
     return $ret
 }
 
 
 Countries[${#Countries[@]}]="Other..."
-chooseintab "Please select your Country of Birth..." Countries
+chooseinlist "Please select your Country of Birth..." "${Countries[@]}"
 ret=$?
 if ((ret==${#Countries[@]})) ; then
     echo -e " Sorry: we can't generate your udid.\n"\
@@ -92,7 +94,7 @@ fi
 if ! LANGUAGE=en $gpg --verify --no-verbose --batch "$GFile" 2>&1 | grep -o "(${GEOLISTUDID[0]}\>.*)" ; then
     #Note: Trust is not checked.
     echo "Warning: the geolist file "$GFile" is not signed by a recognized signature" >&2
-    read -p "The geolist file "$GFile" may provide invalid udid2, do you want to continue ? (y/n)" answer
+    read -p "The geolist file "$GFile" may provide invalid udid2, do you want to continue ? (y/n) " answer
     case "$answer" in
                 Y* | y* | O* | o* )
                 true ;;
@@ -101,6 +103,33 @@ if ! LANGUAGE=en $gpg --verify --no-verbose --batch "$GFile" 2>&1 | grep -o "(${
     esac
 fi
 
-read -p "Please enter your place of birth ? " answer
+for ((j=0;;j++)) ; do 
+    read -p "Please enter your place of birth ? " answer
+    cities="$($gpg --no-verbose --batch --decrypt "$GFile" 2> /dev/null | grep -i "$answer")"
+    eval citiesname=($(echo "$cities" | sed ' s,e[0-9.+-]\+\t[A-Z]\{3\}\t\([^"]\+\).*,"\1",'))
+    
+    chooseinlist "Please validate your place of birth" "${citiesname[@]}" "Other..."
+    ret=$?
+    if ((ret==${#citiesname[@]}+1)) ; then
+        if ((!j)) ; then continue
+        else
+            echo -e " Sorry: we can't generate your udid.\n"\
+            "Please join the OpenUDC's developpement team to add support for your birthplace <open-udc@googlegroups.com>."
+            exit
+        fi
+    else
+        bplace="$(echo "$cities" | sed -n "${ret}p" )"
+        #echo "$bplace" | sed "s,\(e[0-9.+-]\+\)\t[A-Z]\{3\}\t.*,\1,"
+        #echo ${bplace%%$(echo -en "\t")*}
+        break;
+    fi
+done
+
+read -p "Please enter your birth last name (family name) ? " blname
+read -p "Please enter your birth first first name (forename) ? " bfname
+
+read -p "Please enter your date of birth ? (YYYY-MM-DD) " bdate
+date -d "$bdate" "+%A, %d %B %Y"
+
 #eval $gpg --no-verbose --batch --decrypt "$1" 2> /dev/null | 
 
