e7d67f8f1ad0d673587eaaa3ebee31cb3a6d5709
Caner Candan <caner@candan.fr>
2011-07-28 01:21:18 +0200

    * whitespace cleanup in script files

--- scripts/ucreate.sh
+++ scripts/ucreate.sh
@@ -3,7 +3,7 @@
 SW="OpenUDC - devel"
 Version="ucreate.sh 0.0.2 ($SW)"
 
-if [ "$1" == "-V" ] || [ "$1" == "--version" ] ; then 
+if [ "$1" == "-V" ] || [ "$1" == "--version" ] ; then
     echo "$Version"
     exit
 elif [ -z "$1" ] || [ "${1:0:1}" == "-" ] ; then
@@ -33,22 +33,22 @@ fi
 
 for ((i=0;i<48;i++)) ; do
     if ((factors[i])) ; then
-        ((value+=$(echo $(((48-i)%3?(48-i)%3:5))*10^$(((47-i)/3)) | bc -l )*factors[i]))
+	((value+=$(echo $(((48-i)%3?(48-i)%3:5))*10^$(((47-i)/3)) | bc -l )*factors[i]))
     fi
-done 
+done
 
 echo -e "\n Set n°$setnum:\n"\
-        "\tNumber of Individuals: $idnumber\n"\
-        "\tMonetary Dividend: $(echo "scale=2; $value/100" | bc -l ) ${curname}s"
+	"\tNumber of Individuals: $idnumber\n"\
+	"\tMonetary Dividend: $(echo "scale=2; $value/100" | bc -l ) ${curname}s"
 
 # validation is better outside the creation sheet
-#echo -n " Closing date for validation: " 
+#echo -n " Closing date for validation: "
 #if ! date -d "$dlimv" >&2 ; then
 #    echo -e "$SW: Error: file \"$1\" is invalid" >&2
 #    exit -1
 #fi
 
-echo -n " Closing date for creation: " 
+echo -n " Closing date for creation: "
 if ! date -d "$dlimc" >&2 ; then
     echo -e "$SW: Error: file \"$1\" is invalid" >&2
     exit -1
@@ -60,33 +60,33 @@ if (($(date +%s)>$(date -d "$dlimc" +%s))) ; then
 fi
 
 echo
-if gpg2 --version 2> /dev/null | head -n 3 ; then 
+if gpg2 --version 2> /dev/null | head -n 3 ; then
     gpg="gpg2"
 elif gpg --version 2> /dev/null | head -n 3 ; then
     gpg="gpg"
 else
     echo -e "\n No gpg found in your \$PATH ($PATH)\n"\
-            "please install GnuPG (http://www.gnupg.org/)\n" >&2
+	    "please install GnuPG (http://www.gnupg.org/)\n" >&2
     exit -3
 fi
 
 mykeys=($($gpg --list-secret-keys --with-colons | grep "^s" | cut -d: -f 5))
 # Warning: $mykeys contain non-signing key. It is not really annoying, but it's not clean.
 
-if [ -z "$mykeys" ] ; then 
+if [ -z "$mykeys" ] ; then
     echo -e "\n No private key found here. Didn't you forget to create your\n"\
-            "OpenPGP certificat or import the private part here ?\n" >&2
+	    "OpenPGP certificat or import the private part here ?\n" >&2
     exit -4
 fi
 
 myudid2h="$($gpg --list-secret-keys 2> /dev/null | sed -n ' s,.*(\(udid2;h;[[:xdigit:]]\{40\};[0-9]\+\)[;)].*,\1,p ')"
 #myudid2c="$($gpg --list-secret-keys 2> /dev/null | sed -n ' s,.*(\(udid2;c;[A-Z-]\{0,20\};[A-Z-]\{1,20\};[0-9-]\{10\};e[0-9.+-]\{13\};[0-9]\+\)[;)].*,\1,p ')"
 myudid2c="$($gpg --list-secret-keys 2> /dev/null | gawk --re-interval '/\(udid2;c;/ { print gensub(/[^(]*\((udid2;c;[A-Z]{1,20};[A-Z-]{1,20};[0-9-]{10};e[0-9.+-]{13};[0-9]+)[;)].*/, "\\1", "1") }' )"
-myudid1="$($gpg --list-secret-keys 2> /dev/null | sed -n ' s,.*(\(udid1;[^);]\+;[^);]\+;[^);]\+;[^);]\+\)[;)].*,\1,p ' | head -n 1 )" 
+myudid1="$($gpg --list-secret-keys 2> /dev/null | sed -n ' s,.*(\(udid1;[^);]\+;[^);]\+;[^);]\+;[^);]\+\)[;)].*,\1,p ' | head -n 1 )"
 
-if ! [ "$myudid2h" -o "$myudid2c" -o "$myudid1" ] ; then 
+if ! [ "$myudid2h" -o "$myudid2c" -o "$myudid1" ] ; then
     echo -e "\n No udid found in your private datas\n"\
-            "please create a valid udid2 and make it signed by some confidents contacts\n" >&2
+	    "please create a valid udid2 and make it signed by some confidents contacts\n" >&2
     exit -4
 fi
 
@@ -109,47 +109,46 @@ echo -e "\nudid=\"$myudid\" key=$mykey position=$myindex"
 
 if ! echo "${mykeys[*]}" | grep "$mykey" > /dev/null ; then
     echo -e "\n Oups ! the keyID associated with your udid ($mykey) is not\n"\
-            "one of yours here (${mykeys[*]}).\n"\
-            "If there is a mistake in the creation sheet, please alert quickly.\n" >&2
+	    "one of yours here (${mykeys[*]}).\n"\
+	    "If there is a mistake in the creation sheet, please alert quickly.\n" >&2
     exit -5
 fi
 
 while true ; do
     echo
-    read -p "Do you validate this creation set (y/n) ? " rep 
+    read -p "Do you validate this creation set (y/n) ? " rep
     case "$rep" in
       [yY]*)
-        read -t 3 -p "Okay, let's create your part of money !"
-        echo -e "\n"
-        mkdir -p "$HOME/.openudc/$curname/cset"
-        cp -v "$1" "$HOME/.openudc/$curname/cset/cset.$setnum.env"
+	read -t 3 -p "Okay, let's create your part of money !"
+	echo -e "\n"
+	mkdir -p "$HOME/.openudc/$curname/cset"
+	cp -v "$1" "$HOME/.openudc/$curname/cset/cset.$setnum.env"
 
-        #KeyID collision in 2 differents creation sheet time are not managed today
-        mkdir -p "$HOME/.openudc/$curname/$mykey"
-        cfile="$HOME/.openudc/$curname/$mykey/c.$setnum"
-        echo -e "d=t2c\nc=$curname\nh=$(sha1sum -t "$1" | cut -d ' ' -f 1)\nb=(" > "$cfile"
-        for ((i=0;i<48;i++)) ; do
-            if ((factors[i])) ; then
-                value="$(echo $(((48-i)%3?(48-i)%3:5))*10^$(((47-i)/3)) | bc -l )"
-                for ((j=0;j<factors[i];j++)) ; do
-                    echo -n "$value-$setnum-$((factors[i]*myindex+j)).0 "
-                done
-                echo
-            fi
-        done >> "$cfile"
-        echo ")" >> "$cfile"
-        #$gpg --detach-sign -u "${mykey}!" --armor --output - "$HOME/.openudc/$curname/cset/cset.$setnum.env" >> "$cfile"
-        $gpg --sign -u "${mykey}!" "$cfile"
-        # Then we havo to publish this creation... and hurry (before dlimc).
-        break
-        ;;
+	#KeyID collision in 2 differents creation sheet time are not managed today
+	mkdir -p "$HOME/.openudc/$curname/$mykey"
+	cfile="$HOME/.openudc/$curname/$mykey/c.$setnum"
+	echo -e "d=t2c\nc=$curname\nh=$(sha1sum -t "$1" | cut -d ' ' -f 1)\nb=(" > "$cfile"
+	for ((i=0;i<48;i++)) ; do
+	    if ((factors[i])) ; then
+		value="$(echo $(((48-i)%3?(48-i)%3:5))*10^$(((47-i)/3)) | bc -l )"
+		for ((j=0;j<factors[i];j++)) ; do
+		    echo -n "$value-$setnum-$((factors[i]*myindex+j)).0 "
+		done
+		echo
+	    fi
+	done >> "$cfile"
+	echo ")" >> "$cfile"
+	#$gpg --detach-sign -u "${mykey}!" --armor --output - "$HOME/.openudc/$curname/cset/cset.$setnum.env" >> "$cfile"
+	$gpg --sign -u "${mykey}!" "$cfile"
+	# Then we havo to publish this creation... and hurry (before dlimc).
+	break
+	;;
       [nN]*)
-        echo "Okay. If you have not already, please express (to the community) why you disagree."
-        break
-        ;;
+	echo "Okay. If you have not already, please express (to the community) why you disagree."
+	break
+	;;
       *)
-        echo "  please answer \"yes\" or \"no\""
-        ;;
+	echo "  please answer \"yes\" or \"no\""
+	;;
     esac
 done
-
