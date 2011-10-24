700d4444dd9457f0e7d2043776e470d793259f9a
jbar <jeanjacquesbrucker@gmail.com>
2011-10-24 19:45:41 +0200

    works on authentication

--- scripts/main.sh
+++ scripts/main.sh
@@ -11,8 +11,10 @@ UDinit
 while true ; do 
     udc_chooseinlist "Please choose what to do ?" 1 \
                      "Register your udid2 for Universal Monetary Dividend" \
-                     "Register a new key for Universal Monetary Dividend" \
+                     "Change registered key for Universal Monetary Dividend" \
                      "Generate/Check an udid2" \
+                     "Signalize a (living/deceased) individual" \
+                     "Vouch (or no longer vouch) for someone else" \
                      "Synchronyse Monetary creation" \
                      "Check the local balance of your account(s)" \
                      "Make and send a transaction" \
@@ -35,16 +37,30 @@ while true ; do
         read -t 5
         ;;
       4)
-        UDsyncCreation
+        # What is its udid2 ?
+        # When did you see this individual last time ?
+        # Was she/he dead or alive ?
+        # gpg --sign-key -N _dead@-=2011-10-24 -u mykey\! it's_udid2
+        echo "Sorry: Not implemented yet." >&2
+        read -t 5
         ;;
       5)
+        # gpg --sign-key -N _alive@-=2011-10-24 -N '!Iam@voucher=2011-10-12' -u mykey\! it's_udid2
+        echo "Sorry: Not implemented yet." >&2
+        read -t 5
+        ;;
+      6)
+        UDsyncCreation
+        ;;
+      7)
+      [[ "${myaccounts[0]}" ]] || echo "No account found."
         for account in "${myaccounts[@]}" ; do 
             echo -n "$account: "
             udc_freadbalance "$account"
         done
         read -t 5
         ;;
-      6)
+      8)
         if ((${#myaccounts[@]}>1)) ; then
             udc_chooseinlist "From which account ?" 1 "${myaccounts[@]}" >&2
             account="${myaccounts[$(($?-1))]}"
@@ -60,7 +76,12 @@ while true ; do
             echo "There is not enough money" >&2
         else
             if ((ret)) ; then
-                read -p "we miss small grains to reach exactly the amount, do you want to continue (y/n) ? " rep >&2
+                echo "You miss small grains to reach exactly the amount,"
+                if ((ret==253)) ; then 
+                    read -p "  and will send at least $ret more, do you want to continue (y/n) ? " rep >&2
+                else
+                    read -p "  and will send $ret more, do you want to continue (y/n) ? " rep >&2
+                fi
                 case "$rep" in
                   [yY]*)
                     ret=0
@@ -71,26 +92,29 @@ while true ; do
             fi
             if ((!ret)) ; then
                 mapfile < <(sed -n ' s,^[[:xdigit:]]\{40\}:,,p ' "$udcHOME/$Currency/adressbook")
-                udc_chooseinlist "To which account ?" 1 "${MAPFILE[@]}" # "Other..."
+                udc_chooseinlist "To which account ?" 1 "${MAPFILE[@]}" "Other..."
                 destkey=$(sed -n "$?p" <(sed -n ' s,^\([[:xdigit:]]\{40\}\):.*,\1,p ' "$udcHOME/$Currency/adressbook"))
+                [[ "$destkey" ]] || read -p "Enter the fingerprint of destination account: " destkey >&2
                 #echo -n sended_amount
                 echo "$account -> $destkey" >&2
-                udc_maketransaction "$account" "$destkey" "${grains[@]}" >&2
-                # Send it now ...
+                if ! udc_maketransaction "$account" "$destkey" "${grains[@]}" >&2 ; then
+                    echo "Making transaction failed. Return code = $? " >&2
+                #else # Send it now ...
+                fi
             fi
         fi
         ;;
-      7)
+      9)
         echo "Sorry: Not implemented yet." >&2
         read -t 5
         ;;
-      8)
+      10)
         read -p "filename ? " 
         UDvalidate "$REPLY"
         echo "function UDvalidate return $?"
         read -t 5
         ;;
-      9)
+      11)
         break
         ;;
       *)
