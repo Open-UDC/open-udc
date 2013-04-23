aa237d676c9c19e0638d204b9b1b547f6d844f62
Jbar <jeanjacquesbrucker@gmail.com>
2013-04-23 19:54:09 +0200

    update irclogger.sh (no more use for OpenUDC, should be moved in other place)

--- scripts/irclogger.sh
+++ scripts/irclogger.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
+Version="2.0 23Apr2013"
+
+HelpMsg=" Usage: ${0##*/} logpath #channel [[#channel2] ...] server [port]
+
+	Example: ${0##*/} \"/tmp/logs/irc/\" \"#monkeysphere\" irc.oftc.net 6667
+"
+
+IrcComment="logs: http://domesticserver.org/logs/irc/"
+
 function ircbot {
 	# irc.cat.pdx.edu 6667 : Why not ?
 	# irc.oftc.net 6667	: Good
 	# irc.lfnet.org 6667   : The same as bitcoin, solidcoin, namecoin ... ;-)
-
-if (($#<3)) ; then
-	echo -e "\n Usage: $0 logpath channel server [port]\n\n"\
-	"Example: $0 \"/tmp/logs/irc/#monkeysphere/\" \"#monkeysphere\" irc.oftc.net 6667" >&2
-	return 255
-fi
 
 local Tmpdir="$(mktemp --tmpdir -d irclogger_XXX)" || return 251
 trap "rm -rvf \"$Tmpdir\" " EXIT
 
 local Irclogdir="$1"
 mkdir -p "$1" || return 250
+shift
+[ -z "${Irclogdir}" ] && echo -e "Error: invalid logpath argument\n$HelpMsg" >&2 && exit 200
+
+local IrcChans=()
+while [[ ${1:0:1} == '#' ]] ; do
+	IrcChans=("${IrcChans[@]}" $1)
+	mkdir -p "$Irclogdir/${1,,}" || return 250
+	shift
+done
+[ -z "${IrcChans[0]}" ] && echo -e "Error: invalid channel argument\n$HelpMsg" >&2 && exit 200
+
+local IrcServer="$1"
+shift
+[ -z "$IrcServer" ] && echo -e "Error: invalid channel argument\n$HelpMsg" >&2 && exit 200
+
+local IrcPort="${1:-6667}"
+
 local IrcUser="bot"
-local IrcChan="${2:-#udc.test2}"
-local IrcComment="$IrcChan logs: http://..."
 local IrcNickb="_rec_"
 local IrcNick="$IrcNickb$((RANDOM%8388608))"
-local line src command target args
+local line src command target args chan
 
-while true ; do
+while true ; do 
 
 	mkfifo "$Tmpdir/irc.in" || return 249
 	mkfifo "$Tmpdir/irc.out" || return 249
 	trap EXIT # reset trap on EXIT
-	nc "${3:-irc.lfnet.org}" "${4:-6667}" < "$Tmpdir/irc.in" > "$Tmpdir/irc.out" &
+	nc "$IrcServer" "$IrcPort" < "$Tmpdir/irc.in" > "$Tmpdir/irc.out" &
 	trap "sleep 1 ; jobs -l ; jobs -p | xargs kill -9 2> /dev/null ; rm -rvf \"$Tmpdir\" " EXIT
 
 	exec 10> "$Tmpdir/irc.in"
 	exec 11<> "$Tmpdir/irc.out"
-	echo -e "\nUSER $IrcUser 0 _ :$IrcComment\nNICK $IrcNick\nJOIN $IrcChan" >&10
+	echo -e "\nUSER $IrcUser 0 _ :$IrcComment\nNICK $IrcNick" >&10
+	for chan in "${IrcChans[@]}" ; do
+		echo "JOIN $chan" >&10
+	done
 
 	while read -t 510 src command target args <&11 ; do
 		#echo "$(date -R)<- $src $command $target $args"
@@ -41,33 +62,42 @@ while true ; do
 			"PING") echo "PONG $command" >&10 ;;
 			"ERROR") break ;;
 		esac
-		src="${src:1}" # Remove 1st char ':'
+		src="${src:1}" # Remove 1st char ':' 
 		case "$command" in
 			#NOTICE) echo >&10 ;;
-			43[1-6]) IrcNick="$IrcNickb$((RANDOM%8388608))" ; echo -e "NICK $IrcNick\nJOIN $IrcChan" >&10 ;;
+			43[1-6])
+				IrcNick="$IrcNickb$((RANDOM%8388608))"
+				echo "NICK $IrcNick" >&10
+				for chan in "${IrcChans[@]}" ; do
+					echo "JOIN $chan" >&10
+				done
+				;;
 			JOIN|PART|QUIT)
 				SrcNick="${src%%\!*}"
 				tmplogfile="$(date "+%Y/%m/%d").txt"
-				mkdir -p "$Irclogdir/${tmplogfile%/*}"
-				echo "// $(date "+%X%z"): $SrcNick $command // $target" >> "$Irclogdir/$tmplogfile"
+				#To remove f..k.ng \0x0d char ...
+				target=${target//$'\r'}
+				mkdir -p "$Irclogdir/${target,,}/${tmplogfile%/*}"
+				echo "// $(date "+%X%z"): $SrcNick $command // $target" >> "$Irclogdir/${target,,}/$tmplogfile"
 				;;
 			NICK)
 				SrcNick="${src%%\!*}"
 				tmplogfile="$(date "+%Y/%m/%d").txt"
-				mkdir -p "$Irclogdir/${tmplogfile%/*}"
-				echo "// $(date "+%X%z"): $SrcNick is now know as //$target" >> "$Irclogdir/$tmplogfile"
+				for chan in "${IrcChans[@]}" ; do
+					echo "// $(date "+%X%z"): $SrcNick is now know as //$target" >> "$Irclogdir/${chan,,}/$tmplogfile"
+				done
 				;;
 			PRIVMSG)
 				SrcNick="${src%%\!*}"
-				case "$target" in
-				  "$IrcChan")
-						tmplogfile="$(date "+%Y/%m/%d").txt"
-						mkdir -p "$Irclogdir/${tmplogfile%/*}"
-						echo "$(date "+%X%z"): $SrcNick $args" >> "$Irclogdir/$tmplogfile"
-						#echo "Message in main chan from $SrcNick: $(echo "${args}" | hexdump -C)" >&2
-					   ;;
-				  "$IrcNick") ((!(RANDOM%4))) && echo "$(fortune)" | while read line ; do echo "PRIVMSG $SrcNick :$line" ; done >&10 ;;
-				esac
+				if [[ "$target" == $IrcNick ]] ; then
+					((!(RANDOM%4))) && echo "$(fortune)" | while read line ; do echo "PRIVMSG $SrcNick :$line" ; done >&10
+				else
+					tmplogfile="$(date "+%Y/%m/%d").txt"
+					mkdir -p "$Irclogdir/${target,,}/${tmplogfile%/*}"
+					echo "$(date "+%X%z"): $SrcNick $args" >> "$Irclogdir/${target,,}/$tmplogfile"
+				fi
+				#echo "Message in main chan from $SrcNick: $(echo "${args}" | hexdump -C)" >&2
+				;;
 		esac
 	done
 	exec 10>&- 11>&-
@@ -78,5 +108,18 @@ while true ; do
 done
 }
 
-ircbot $@
+# Save parameters 
+args=("$@")
+
+for ((i=0;$#;)) ; do
+	case "$1" in
+		-h|--h*) echo "$HelpMsg" ; exit ;;
+		-V|--vers*) echo "${0##*/} $Version" ; exit ;;
+		-*) echo -e "Error: Unrecognized option $1 \n$HelpMsg" >&2 ; exit 200 ;;
+	esac
+	shift
+done
+
+ircbot "${args[@]}"
 exit $?
+
